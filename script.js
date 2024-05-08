9503d3e4b4b5b400dd4a2afa79be27c87ccce696
nomthandazo <ndlovunomthandazo4@gmail.com>
2024-05-08 14:57:07 +0200

    Okay

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-let inpt = document.getElementById("inptFile")
-let tableD = document.getElementById("btl")
+const inpt = document.getElementById("inptFile")
+const tableD = document.getElementById("btl")
 
 // function loadF(){
 
 // }
+inpt.addEventListener('change', (event) => {
+    const file = event.target.files[0]
+
+    readXlsxFile(file).then(rows) => {
+        console.log(rows)
+        tableD.innerHTML = ""
+
+        rows.forEach((row)=> {
+            let tr = document.createElement('tr')
+            rows.forEach((cell)=> {
+                const td = document.createElement('td')
+                td.textContent = cell 
+                tr.appendChild(td)
+        })
+         table.appendChild(tr)   
+        })
+    }
+
+})
